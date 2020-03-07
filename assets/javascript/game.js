7e9203be1b67547a21560b6022ad5ca73d59cfd9
Joe Cooler <joe_cooler@msn.com>
2020-03-07 13:27:56 -0600

    started javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,26 @@
 // initialize game
 
-document.onkeypress = startGame;
+document.addEventListener ("keypress", startGame);
 
 function startGame () {
-    alert ("blah");
+
+        
+    }
+
     
     // do some stuff here
+}
+
+function resetText () {
+    var gameText  = document.getElementsByClassName ("gText");
+
+    gameText[0].textContent = "_ _ _ _ _ _ _ _";
+    gameText[1].textContent = "Guesses left: 0 ";
+    gameText[2].textContent = "Characters used: ";
+}
+
+function pickMovie () {
+    var movList = ["Goodfellas", "Pulp Fiction", "Reservoir Dogs", "Casino", "The Usual Suspects", "Office Space", "Forrest Gump", "The Shawshank Redemption", "Fight Club", "Seven"]
+    var rNum = Math.floor((Math.random() * 9) + 0);
+    return movList[rNum];
 }
